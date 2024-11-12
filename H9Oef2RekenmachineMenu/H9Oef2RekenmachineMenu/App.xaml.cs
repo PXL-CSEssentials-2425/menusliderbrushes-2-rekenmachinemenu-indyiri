06f247e7d66449f6836148930eb733e0795e61cf
Indy Iri <indy.iri@student.pxl.be>
2024-11-12 14:19:02 +0100

    WPF aangemaakt

---
+++ H9Oef2RekenmachineMenu/H9Oef2RekenmachineMenu/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace H9Oef2RekenmachineMenu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
